23875f67605a376d95f61e534ec4c41d5ef00820
loquat01 <iida01@yahoo.co.jp>
2023-11-09 16:51:46 +0900

    Update ebu.rb 4.5-20220808

--- ebu.rb
+++ ebu.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Ebu < Formula
   homepage "http://green.ribbon.to/~ikazuhiro/dic/ebu.html"
-  url "http://green.ribbon.to/~ikazuhiro/dic/files/ebu-4.5-20200413.tar.gz"
-  version "4.5-20200413"
-  sha256 "bd9ceaef2a0a43049aead871b3bf9b56cadee59ad2478b13c77130fe8694c1f1"
+  url "http://green.ribbon.to/~ikazuhiro/dic/files/ebu-4.5-20220808.tar.gz"
+  version "4.5-20220808"
+  sha256 "df72d09a937dcc91586eff5b9e4d18b9d5d80bc78dd74e541b052f3ebe92a2a5"
 
   def install
     system "./configure", "--disable-debug",
