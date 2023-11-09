5b9c3f13211d38ec211b1bd620cceaa8cfae1d9b
loquat01 <iida01@yahoo.co.jp>
2023-11-09 17:30:18 +0900

    CheckSum fix

--- ebu.rb
+++ ebu.rb
@@ -4,7 +4,7 @@ class Ebu < Formula
   homepage "http://green.ribbon.to/~ikazuhiro/dic/ebu.html"
   url "http://green.ribbon.to/~ikazuhiro/dic/files/ebu-4.5-20220808.tar.gz"
   version "4.5-20220808"
-  sha256 "df72d09a937dcc91586eff5b9e4d18b9d5d80bc78dd74e541b052f3ebe92a2a5"
+  sha256 "374e90f8738d0ffc7a1e2f3d1c2be70626135f89aeb0656bc7ab0a0eb66f5b89"
 
   def install
     system "./configure", "--disable-debug",
